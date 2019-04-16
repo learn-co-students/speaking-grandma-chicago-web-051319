34918a2633447fdeb11f88aca764fa1346c6b606
Emily R <emilyroulund@gmail.com>
2019-04-16 22:49:30 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,9 @@
 def speak_to_grandma (string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-      elsif string != "string".upcase
-    return "NO, NOT SINCE 1938!"
+  elsif string != string.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
   else 
-   return "HUH?! SPEAK UP, SONNY!"
+   return "NO, NOT SINCE 1938!"
+ end
 end
